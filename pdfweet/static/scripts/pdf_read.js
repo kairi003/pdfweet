6967886fcf57a38300abf59bee57b17d2193d802
kair_003 <kairi.satellite@gmail.com>
2019-10-26 19:09:56 +0900

    remove_dup_js

--- pdfweet/static/scripts/pdf_read.js
+++ pdfweet/static/scripts/pdf_read.js
@@ -1,21 +1,18 @@
 pdfjsLib.GlobalWorkerOptions.workerSrc = '/static/scripts/pdf.worker.js';
 
+window.blobs = [];
+
 const pdf2canvas = async (pdf, i) => {
   const page = await pdf.getPage(i);
   const scale = 1;
-  const viewport = page.getViewport({
-    scale: scale,
-  });
+  const viewport = page.getViewport({scale});
   const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d');
+  const canvasContext = canvas.getContext('2d');
 
   canvas.height = viewport.height;
   canvas.width = viewport.width;
 
-  const renderContext = {
-    canvasContext: context,
-    viewport: viewport,
-  };
+  const renderContext = {canvasContext, viewport};
   await page.render(renderContext);
   return canvas;
 };
@@ -26,8 +23,7 @@ const pdf2blob = async (pdf, i) => {
   return blob;
 };
 
-const pdf2img = async (pdf, i) => {
-  const blob = await pdf2blob(pdf, i);
+const blob2img = blob => {
   const img = Object.assign(new Image(), {
     className: 'page-img',
     src: URL.createObjectURL(blob)
@@ -36,11 +32,11 @@ const pdf2img = async (pdf, i) => {
 };
 
 const pdf_load = async (url, password) => {
+  window.blobs = [];
+  const pdfView = document.getElementById('pdfView');
+  document.querySelectorAll('.view-item').forEach(item=>pdfview.removeChild(item));
   for (let img of document.querySelectorAll('.page-img')) img.parentNode.removeChild(img);
-  const loadingTask = pdfjsLib.getDocument({
-    url,
-    password
-  });
+  const loadingTask = pdfjsLib.getDocument({url,　password});
   const pdf = await loadingTask.promise.catch(err => err);
   if (pdf instanceof Error) {
     const err = pdf;
@@ -53,15 +49,15 @@ const pdf_load = async (url, password) => {
     }
   }
   const range = [...Array(pdf.numPages).keys()];
-  const imgs = await Promise.all(range.map(async i => pdf2img(pdf, i + 1)));
-  const pdfview = document.getElementById('pdfView');
-  for (let img of imgs) {
+  window.blobs = await Promise.all(range.map(async i => pdf2blob(pdf, i + 1)));
+  for (let blob of window.blobs) {
+    let img = blob2img(blob);
     img.className = 'pdf-image';
     const viewitem = Object.assign(document.createElement('li'), {
-      class: 'view-item'
+      className: 'view-item'
     });
     viewitem.append(img);
-    pdfview.append(viewitem);
+    pdfView.append(viewitem);
   }
 };
 
@@ -72,6 +68,20 @@ const pdf_input = async () => {
   pdf_load(pdf_url, pdf_password);  
 }
 
-pdfFile.addEventListener('change', pdf_input);
-pdfPassword.addEventListener('change', pdf_input);
-
+document.getElementById('pdfFile').addEventListener('change', pdf_input);
+document.getElementById('pdfPassword').addEventListener('change', pdf_input);
+document.getElementById('postTweet').addEventListener('click', ()=>{
+  if (window.blobs.length == 0) {
+    alert('PDFファイルがありません');
+    return false;
+  }
+  const formData = new FormData();
+  for (let i in window.blobs) formData.append(`image[${i}]`, window.blobs[i]);
+  formData.append('num', window.blobs.length.toString());
+  formData.append('text', document.getElementById('tweetText').value);
+  fetch('/tweet', {method: 'POST', body: formData}).then(r=>r.text()).then(html=>{
+    document.getElementById('tweetView').innerHTML = html;
+    window.twirender();
+  });
+  alert('ツイートを送信しました');
+})
