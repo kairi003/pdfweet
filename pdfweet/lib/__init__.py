a5e79b093c7a9c00db478e9e86b882d687c40ad5
kairi003 <kairi.satellite@gmail.com>
2022-10-13 19:14:57 +0900

    [update] add shebang

--- pdfweet/lib/__init__.py
+++ pdfweet/lib/__init__.py
@@ -1,1 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from pdfweet.lib import TwitterHandler
--- pdfweet/views.py
+++ pdfweet/views.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import json
 from flask import session, redirect, render_template, request, url_for
 
